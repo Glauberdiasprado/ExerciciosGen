7c22e54e55d3d38c532dae1bf3a29640212051f1
Glauberdiasprado <glauberdiasprado@gmail.com>
2022-06-08 12:29:33 -0300

    exerciciofor02

---
+++ eclipse-workspace/JavaExercicio/src/br/generation/lacosrepeticao/ExercicioFor02.java
@@ -0,0 +1,32 @@
+package br.generation.lacosrepeticao;
+
+import java.util.Scanner;
+
+public class ExercicioFor02 {
+
+	/*
+	 * 2- Ler 10 números e imprimir quantos são pares e quantos são ímpares. (FOR)
+	 */
+	
+    public  static  void  main ( String [] argss ){
+        Scanner  in = new  Scanner ( System . in );
+        int  num , par = 0 , impar = 0 ;
+
+        for ( int  i = 1 ; i <= 10 ; i ++){
+            System.out.println( "Digite o " + i + "º número" );
+            num = in.nextInt ();
+
+            if ( num % 2 == 0 ){
+                par++;
+            }
+            if ( num % 2 == 0 ){
+                impar++;
+            }
+
+            System.out.println();
+        }
+
+        System.out.println( "Foram digitados " + par + " números pares \n" );
+        System.out.println( "Foram digitados " + impar+ " números impares \n" );
+    }
+}
